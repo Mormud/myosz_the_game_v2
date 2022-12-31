3646d2b746935716fb911ef24922b35a9e4b5c53
Mormud <misiak.myosz@gmail.com>
2022-12-31 17:35:26 +0100

    poprawiona wydajnosc silnika mapy i dodanie funkcjonalnosci
    zrobione:
    - poruszanie sie po mapie
    - przyblizanie i oddalanie
    - zoptymalizowanie silnika mapy
     - dodanie przyciskow funkcyjnych w menu tworzenia mapy

--- create_map.py
+++ create_map.py
@@ -18,15 +18,26 @@ class CreateMap():
         self.running = True
         self.selected_item = 0
         self.plr_color = plr_color_list
+
+        # map
         self.map_width = int(screen_size[0] - self.btn_x_vector * 2)
         self.map_height = screen_size[1]
-        self.map = Map()
+        self.field_size = 30
+        self.map = Map(self.field_size)
+        self.zoom_value = 5
 
         # screen elements
+        # right menu
         self.back_btn = Button('Wróć', 200, 40, ((self.btn_x_vector * 6) + 70, (self.btn_y_vector)),
                                5, self.screen, self.gui_font)
-        self.choose_element = Button('Gracz', 200, 40, ((self.btn_x_vector * 6) + 70, self.btn_y_vector * 2),
+        self.zoom_in_btn = Button('+', 40, 40, ((self.btn_x_vector * 6) + 150, (self.btn_y_vector * 2)),
+                                  5, self.screen, self.gui_font)
+        self.zoom_out_btn = Button('-', 40, 40, ((self.btn_x_vector * 6) + 150, (self.btn_y_vector * 3)),
+                                   5, self.screen, self.gui_font)
+        self.choose_element = Button('Gracz', 200, 40, ((self.btn_x_vector * 6) + 70, self.btn_y_vector * 4),
                                      5, self.screen, self.gui_font)
+        self.clear_btn = Button('Reset', 200, 40, ((self.btn_x_vector * 6) + 70, (self.btn_y_vector * 12)),
+                               5, self.screen, self.gui_font)
         self.right_menu_bg_surf = pygame.Surface((screen_size[0] - self.map_width, screen_size[1]))
         self.right_menu_bg_surf.fill('#202124')
         self.right_menu_bg_rect = self.right_menu_bg_surf.get_rect(bottomright=(screen_size[0], screen_size[1]))
@@ -43,13 +54,31 @@ class CreateMap():
             self.back_btn.draw()
             if self.back_btn.check_click():
                 self.running = False
+
+            self.zoom_in_btn.draw()
+            if self.zoom_in_btn.check_click():
+                if self.field_size + self.zoom_value < 50:
+                    self.field_size += self.zoom_value
+                    self.map.zoom_in(self.field_size)
+
+            self.zoom_out_btn.draw()
+            if self.zoom_out_btn.check_click():
+                 if self.field_size - self.zoom_value > 10:
+                    self.field_size -= self.zoom_value
+                    self.map.zoom_out(self.field_size)
+
             self.choose_element.draw()
             if self.choose_element.check_click():
                 self.selected_item += 1
                 if self.selected_item > len(self.plr_color) - 1:
                     self.selected_item = 0
+
+            self.clear_btn.draw()
+            if self.clear_btn.check_click():
+                self.map.clear_map()
+
             # selected item label
-            selected_lbl = Label('', 200, 45, ((self.btn_x_vector * 6) + 70, self.btn_y_vector * 3), self.screen, self.gui_font,self.plr_color[self.selected_item])
+            selected_lbl = Label('', 200, 45, ((self.btn_x_vector * 6) + 70, self.btn_y_vector * 5), self.screen, self.gui_font,self.plr_color[self.selected_item])
             selected_lbl.draw()
 
             # main loop
--- map_engine.py
+++ map_engine.py
@@ -6,21 +6,23 @@ from plr_color import plr_color_list
 
 
 class Map:
-    def __init__(self):
+    def __init__(self, field_size):
         # map variables
         self.num_x_fields = 50
         self.num_y_fields = 30
         self.map = numpy.zeros((self.num_x_fields,self.num_y_fields), numpy.dtype(int))
-        self.field_size = 30
+        self.field_size = field_size
         self.map_surf = pygame.Surface((self.num_x_fields * self.field_size, self.num_y_fields * self.field_size))
 
         # engine variables
         self.map_generated = False
         self.offset_x = 0
         self.offset_y = 0
-        self.default_moving_speed = 5
+        self.default_moving_speed = 8
         self.moving_speed = 5
         self.moving_speed_switched = False
+        self.draw_on_map_clicked = False
+        self.temp_fields_list = []
 
         # others var
         self.plr_colors = plr_color_list
@@ -41,11 +43,28 @@ class Map:
         mouse_pos = pygame.mouse.get_pos()
         if mouse_pos[0] < width_map and mouse_pos[1] < height_map:
             if pygame.mouse.get_pressed()[0]:
+                self.draw_on_map_clicked = True
                 x_axis = math.floor((mouse_pos[0] - self.offset_x) / self.field_size)
                 y_axis = math.floor((mouse_pos[1] - self.offset_y) / self.field_size)
-                self.map[x_axis][y_axis] = selected_item
-                self.map_generated = False
+                if x_axis < self.num_x_fields and y_axis < self.num_y_fields:
+                    self.map[x_axis][y_axis] = selected_item
+                    self.temp_fields_list.append((x_axis, y_axis, selected_item))
+                    self.map_generated = False
 
+            try:
+                self.temp_fields_list = list(dict.fromkeys(self.temp_fields_list))
+                for field in self.temp_fields_list:
+                    single_field_surf = pygame.Surface((self.field_size, self.field_size))
+                    single_field_surf.fill(field[2])
+                    self.map_surf.blit(single_field_surf, (field[0], field[1]))
+            except:
+                pass
+
+            if self.draw_on_map_clicked:
+                if pygame.mouse.get_pressed()[0] == False:
+                    self.temp_fields_list.clear()
+                    self.map_generated = False
+                    self.draw_on_map_clicked = False
 
     def moving_on_map(self):
         if pygame.key.get_pressed()[pygame.K_LSHIFT]:
@@ -63,6 +82,21 @@ class Map:
         if pygame.key.get_pressed()[pygame.K_a]:
             self.offset_x += self.moving_speed
 
+    def zoom_in(self, field_size):
+        self.field_size = field_size
+        self.map_surf = pygame.Surface((self.num_x_fields * self.field_size, self.num_y_fields * self.field_size))
+        self.map_generated = False
+
+    def zoom_out(self, field_size):
+        self.field_size = field_size
+        self.map_surf = pygame.Surface((self.num_x_fields * self.field_size, self.num_y_fields * self.field_size))
+        self.map_generated = False
+    def get_map(self):
+        return self.map
+
+    def clear_map(self):
+        self.map = numpy.zeros((self.num_x_fields,self.num_y_fields), numpy.dtype(int))
+        self.map_generated = False
 
     def create_map_func(self, surface, selected_item, width_map, height_map):
         t1 = threading.Thread(target=self.generate_map)
